d55f8cb25a30d2d888b09439c4e41f11269ef142
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-09-05 09:31:15 +0900

    blogモデルにおいて不要なバリデーションメソッドを削除、プレースホルダー名をtermに変更

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -6,23 +6,16 @@ class Blog < ApplicationRecord
   has_many :liking_users, class_name: 'User', source: :user, through: :likings
 
   validates :title, :content, presence: true
-  validate :random_eyecatch_cannot_be_used_if_user_is_not_premium
 
   scope :published, -> { where('secret = FALSE') }
 
   scope :search, lambda { |term|
-    where('title LIKE :item OR content LIKE :item', item: "%#{term}%")
+    where('title LIKE :term OR content LIKE :term', term: "%#{term}%")
   }
 
   scope :default_order, -> { order(id: :desc) }
 
   def owned_by?(target_user)
     user == target_user
-  end
-
-  private
-
-  def random_eyecatch_cannot_be_used_if_user_is_not_premium
-    errors.add(:random_eyecatch, 'ランダムアイキャッチ画像はPremiumユーザーの特典です。') if random_eyecatch && !user.premium?
   end
 end
